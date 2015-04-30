43eb31c76242d86b618137a4b387dc12c08c288c
Overxfl0w <overxfl0w13@hotmail.com>
2015-04-30 18:48:02 +0200

    Update postings_operations.py

--- postings_operations.py
+++ postings_operations.py
@@ -5,13 +5,37 @@
 class postings_operations:
 	
 	@staticmethod
-	def _intersection(l1,l2): return list(set(l1).intersection(set(l2)))
+	def _intersection(l1,l2): 
+		lista,i,j = [],0,0
+		while(i<len(l1) and j<len(l2)):
+			if (l1[i][1] == l2[j][1]): lista.append(l1[i]); j+=1; i+=1
+			elif(l1[i][1] < l2[j][1]): i+=1
+			else: j+=1
+		return lista
 	
 	@staticmethod
-	def _union(l1,l2): return list(set(l1).union(set(l2)))
+	def _union(l1,l2): 
+		lista,i,j = [],0,0
+		while(i<len(l1) and j<len(l2)):
+			if (l1[i][1] == l2[j][1]): lista.append(l1[i]); j+=1; i+=1
+			elif(l1[i][1] < l2[j][1]): lista.append(l1[i]); i+=1
+			else: lista.append(l2[j]); j+=1
+		if(i==len(l1)): 
+			while(j<len(l2)): lista.append(l2[j]); j+=1
+		else: 
+			while(i<len(l1)): lista.append(l1[i]);i+=1
+		return lista
 	
 	@staticmethod
-	def _not_intersection(l1,l2): return list(set(l1).difference(set(l2)))				
+	def _not_intersection(l1,l2): 
+		lista,i,j = [],0,0
+		while(i<len(l1) and j<len(l2)):
+			if (l1[i][1] == l2[j][1]): j+=1; i+=1
+			elif(l1[i][1] < l2[j][1]): lista.append(l1[i]); i+=1
+			else: j+=1
+		if(j==len(l2)): 
+			while(i<len(l1)): lista.append(l1[i]);i+=1
+		return lista			
 	
 	@staticmethod
 	def _parse_operation(op,l1,l2):
