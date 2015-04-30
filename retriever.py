79cfb636cc2109c85d674b2b576d7c2d6b35597e
Overxfl0w <overxfl0w13@hotmail.com>
2015-04-30 18:47:33 +0200

    Update and rename retrieval.py to retriever.py

--- retriever.py
+++ retriever.py
@@ -2,21 +2,22 @@
 # -*- coding: utf-8 -*-
 # Authors: Jose & Alberto #
 
-from sys import argv
+from sys import argv,stdin
 try: import cPickle as pickle
 except: import pickle
 from postings_operations import postings_operations 
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
-from re import findall
+from re import sub,findall
+from time import time
 
-# Arreglar problemas UNICODE #
-# Solucionar dudas con índice y stems en el mismo fichero #
+STOPWORDS = [x for x in stopwords.words('spanish')]
+STEMMER   = SnowballStemmer("spanish")
 
 # USAGE #
 def usage(): print """Usage: python retrieval.py fichero_indice opciones\n
     -> fichero_indice: Indica el fichero de donde se cargará el indice
-    -> opciones: Indica si se eliminaran stopwords (0), stemming(1) o los 2 (2)\n"""
+    -> opciones: Indica si no se hara nada (0), stopwords (1), stemming(2) o los 2 (3)\n"""
 
 def load_object(source):
 	with open(source,'rb') as fd:
@@ -29,6 +30,7 @@ def load_object(source):
 	# index[3] -> notice-date-index: lo mimso pero con fecha #
 	# index[4] -> notice-category-index: lo mismo pero sobre categoria #
 	# index[5] -> notice-hash: buscas posicion de noticia- informacion asociada (titulo,...) #
+	# index[6] -> notice-text: texto de la noticia #
 	
 def take_posting_list(term, search,index):
 	obj = []
@@ -41,10 +43,10 @@ def take_posting_list(term, search,index):
 	finally: return obj
 	
 # Eliminar stopwords de la query#
-def remove_stopwords(text, language='spanish'): return [w for w in text if w.lower() not in stopwords.words(language)]
+def remove_stopwords(text, language='spanish'): return [w for w in text if w.lower() not in STOPWORDS]
 
 # Hacer stemming a cada palabra de la query #
-def make_stemming(text,stemmer): return [stemmer.stem(word) for word in text if word!="headline" and word!="date"] # Parche guarro, apañar #
+def make_stemming(text,stemmer): return [stemmer.stem(word) for word in text if word!="headline" and word!="date"]
 
 #Comprobar si la query tiene todas las operaciones necesarias, y devolver la query correspondiente #
 def correct_operations(query):
@@ -64,7 +66,6 @@ def correct_operations(query):
 #Comprobar si la query está correctamente formada, y devolver la query correcta #
 def take_correct_query(query):
 	query_correct,term1,term2 = [],"",""
-	#print query
 	while(not query==[]):
 		term1 = query.pop(0)
 		if(not query==[] and term1=="headline" or term1=="text" or term1=="category" or term1=="date"):
@@ -76,50 +77,43 @@ def take_correct_query(query):
 				if(not query==[]):query_correct.append(query.pop(0))
 				if(not query==[] and query[0]=="not"): query_correct.append(query.pop(0))
 		elif(term1=="and" or term1=="or"):
-			if(not query==[] and query[0]=="not"): query.pop(0)
+			if(query_correct[-1]== "and" or query_correct=="or"):
+				if(not query==[] and query[0]!="and" and query[0]!="or"): query_correct.append(query.pop(0))
+				else: query.pop(0)
 		else: 
 			query_correct.append(term1)
 			if(not query==[]):query_correct.append(query.pop(0))
 			if(not query==[] and query[0]=="not"): query_correct.append(query.pop(0))
 	return query_correct
-		
-def get_notice_text(file_text,notice_title):
-	notice_pos        = file_text.find(notice_title)
-	# Extraer inicio del texto de la noticia #
-	notice_text_start = notice_pos+len(notice_title)+len("</TITLE>")+len("</TEXT>")+1
-	# Extraer final del texto de la noticia #
-	notice_text_end   = notice_text_start+file_text[notice_text_start:].find("</TEXT>")
-	return file_text[notice_text_start:notice_text_end].lower()
 	
-def show_results(posting_list_result,index,query_terms):
+def show_results(posting_list_result,index,query_terms,query_time):
 	# Si no hay resultados, mostrar mensaje de no resultados #
 	if len(posting_list_result)==0:
 		print "[-] There aren't results for terms: ",query_terms
 		
 	# Si solo hay una o 2 noticias relevantes, mostrar el título y el cuerpo de la noticia #
 	elif len(posting_list_result) in range(1,3):
-		for noticeid in posting_list_result:
-			notice_file,notice_title = index[0][noticeid[0]],index[5][noticeid[1]][0][1]
-			with open(notice_file,"r") as fd: print "Notice -> ",notice_title,"\n",get_notice_text(fd.read(),notice_title),"\n\n"
+		for noticeid in posting_list_result: print index[5][noticeid[1]][0][1],"\n",index[6][noticeid],"\n\n"
 								
 	# Si hay entre 3 y 5 noticias relevantes, mostrar el título y un snippet del cuerpo que contenga los términos #
 	elif len(posting_list_result) in range(3,7):
 		context= 5
 		for noticeid in posting_list_result:
-			notice_file,notice_title = index[0][noticeid[0]],index[5][noticeid[1]][0][1]
-			with open(notice_file,"r") as fd:
-				print "Notice -> ",notice_title
-				notice_text = get_notice_text(fd.read(),notice_title)
-				notice_text = findall(r"[\w']+",notice_text)
-				# Mostrar snippet de cada termino (¿Alternativas?)#
-				for term in query_terms:
-					try:
-						term_pos      = notice_text.index(term)
-						left_context  = term_pos-context if term_pos-context>=0 else 0
-						right_context = term_pos+context
-						print "Snippet de",term,"->"," ".join(notice_text[left_context:right_context])+"\n"
-					except ValueError as ve:
-						print "Snippet de",term,"-> No encontrado en el cuerpo de la noticia \n"
+			notice_title = index[5][noticeid[1]][0][1]
+			print notice_title,"\n"
+			notice_text = index[6][noticeid]
+			notice_text = findall(r"[\w']+",notice_text)
+			max_pos,min_pos = 0,float('inf')
+			for term in query_terms:
+				try:
+					term_pos = notice_text.index(term)
+					if term_pos > max_pos: max_pos = term_pos
+					if term_pos < min_pos: min_pos = term_pos
+				except ValueError as ve: print ve
+				
+			left_context  = min_pos-context if min_pos-context>=0 else 0
+			right_context = max_pos+context
+			print " ".join(notice_text[left_context:right_context])+"\n\n"
 									
 	# Si hay más de 5 noticias relevantes, mostrar el título de las 10 primeras #
 	else:
@@ -128,7 +122,7 @@ def show_results(posting_list_result,index,query_terms):
 			if c>10: break;
 			print index[5][noticeid[1]][0][1] # Mostrar titulo	
 			c += 1
-
+	print len(posting_list_result),"results in",query_time,"ms\n"
 # Extrae los terminos de la consulta #
 def extract_query_terms(query):
 	terms = []
@@ -137,26 +131,32 @@ def extract_query_terms(query):
 			if term not in ["and","or","not"]: terms.append(term)
 		else: terms.append(term.split(":")[1])
 	return terms
-	
+
+# Elimina caracteres no alfanumericos #
+def delete_non_alphanumeric(text): return sub(u"[^\wáéíóúñçàèìòù]",u" ",text)
+
 def retriever(index_file, deleting):
 	query,term1,term2,op,posting_list_result,posting_list_aux,search1,search2 = None,"","","",[],[],"",""
-	index = load_object(index_file)
+	index = load_object(index_file)	
 	while(True):
-		query = raw_input("What are you looking for? -> ").replace(":"," ").split(" ")
-		query = correct_operations(query)
+		query = delete_non_alphanumeric(raw_input("What are you looking for? -> ").replace(":"," ").lower().decode("utf-8")).split(" ")		
 		print "\n"
-		if(deleting == 1):   query = remove_stopwords(query)
-		elif(deleting == 2): query = make_stemming(query, SnowballStemmer("spanish"))
-		elif(deleting == 3): query = make_stemming(remove_stopwords(query), SnowballStemmer("spanish"))
+		init_time = time()		
+		query = correct_operations(query)
+		print "Correct query:",query
+		if(deleting   == 1): query = remove_stopwords(query)
+		elif(deleting == 2): query = make_stemming(query, STEMMER)
+		elif(deleting == 3): query = make_stemming(remove_stopwords(query), STEMMER)
 		if(query==['']): print "[-] You are looking for nothing.\n"; break
 		query = take_correct_query(query)
+		print "Processed query:",query		
 		query_terms = extract_query_terms(" ".join(query))
-		if query==[]: print "[-] Words removed because they are stopwords, throw another query.\n" ; continue			
+		if query==[]: print "[-] Words removed because they are stopwords, run another query.\n" ; continue			
 		term1 = query.pop(0)
 		if(":" in term1): 
 			search1 = term1.split(":")[0]
 			term1 = term1.split(":")[1]
-		else: search1 = ""
+		else: search1 = "text"
 		posting_list_result = take_posting_list(term1,search1,index)
 		while(not query == []):			
 			op = query.pop(0);
@@ -165,13 +165,13 @@ def retriever(index_file, deleting):
 			if(":" in term2): 
 				search2 = term2.split(":")[0]
 				term2 = term2.split(":")[1]
-			else : search2 = ""
+			else : search2 = "text"
 			posting_list_aux = take_posting_list(term2,search2,index)
 			posting_list_result = postings_operations._parse_operation(op,posting_list_result, posting_list_aux)
-		show_results(posting_list_result,index,query_terms)
+		end_time = time()
+		show_results(posting_list_result,index,query_terms,(end_time-init_time))
 
 # Entry point #
 if __name__ == "__main__":
 	if len(argv)<3 or int(argv[2]) not in [0,1,2,3]: usage(); exit()
 	retriever(argv[1],int(argv[2]))
-	#retriever("indexfile",1)
